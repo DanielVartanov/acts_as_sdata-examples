2142e687475dd1d03dfa240bd32ec1130c206df8
Daniel Vartanov <daniel.vartanov@gmail.com>
2010-01-09 16:17:33 +0600

    Fixed wrong validation condition

--- presidents/app/models/president.rb
+++ presidents/app/models/president.rb
@@ -1,6 +1,6 @@
 class President < ActiveRecord::Base
   validates_presence_of :order
-  validates_uniqueness_of :order
+  validates_uniqueness_of :order, :scope => [:country]
 
   acts_as_sdata :title => lambda { "#{last_name}, #{first_name}" },
                 :summary => lambda { "#{last_name}, #{first_name} (#{born_at}-#{died_at})" },
