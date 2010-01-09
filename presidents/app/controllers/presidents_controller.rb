a586a9f375230a9d0a5205d4cb2e15ae6473efab
Daniel Vartanov <daniel.vartanov@gmail.com>
2010-01-09 20:38:41 +0600

    Publishing enabled

--- presidents/app/controllers/presidents_controller.rb
+++ presidents/app/controllers/presidents_controller.rb
@@ -8,13 +8,4 @@ class PresidentsController < ApplicationController
   def index
     @presidents = President.all
   end
-
-  def create
-    president = President.new(params[:entry].to_attributes)
-    if president.save
-      render :xml => president.to_atom.to_xml, :status => :created
-    else
-      render :xml => president.errors.to_xml, :status => :bad_request
-    end
-  end
 end
--- presidents/config/routes.rb
+++ presidents/config/routes.rb
@@ -1,7 +1,5 @@
-#ActionController::Routing::Routes.draw(:delimiters => ['/', '.', '!', '\(', '\)' ]) do |map|
-ActionController::Routing::Routes.draw do |map|
-#  map.resources :presidents
-#  map.connect '/presidents(.:format)', :controller => 'presidents', :action => 'index'
-  map.resource :presidents
-#  map.sdata_resource :presidents #, :formatted_collection_path => true
+ActionController::Routing::Routes.draw(:delimiters => ['/', '.', '!', '\(', '\)' ]) do |map|
+#ActionController::Routing::Routes.draw do |map|
+#  map.resource :presidents
+  map.sdata_resource :presidents #, :formatted_paths => true
 end
