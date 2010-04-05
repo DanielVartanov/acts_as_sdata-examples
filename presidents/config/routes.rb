68753a4e1696adddd0aa3d8c4579526724dee42a
Daniel <daniel@daniel-workstation.(none)>
2010-04-05 16:04:38 +0600

    Removing abdunant comment

--- presidents/config/routes.rb
+++ presidents/config/routes.rb
@@ -1,5 +1,4 @@
 ActionController::Routing::Routes.draw(:delimiters => ['/', '.', '!', '\(', '\)' ]) do |map|
-#ActionController::Routing::Routes.draw do |map|
 #  map.resource :presidents
   map.sdata_resource :presidents #, :formatted_paths => true
 end
