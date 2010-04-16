e7866b9f7c5581e0f5e416712a3545e120086798
eugene gilburg <eugene@richard-test2.(none)>
2010-04-15 20:40:38 -0700

    updated test app to work with latest acts as sdata plugion

--- trading-accounts/app/controllers/s_data/trading_accounts_controller.rb
+++ trading-accounts/app/controllers/s_data/trading_accounts_controller.rb
@@ -2,6 +2,8 @@ class SData::TradingAccountsController < ApplicationController
   acts_as_sdata :model => TradingAccount,
                 :feed => {:author => 'Billing Boss',
                           :path => '/trading_accounts', #what is this parameter for?
-                          :title => 'Billing Boss | Trading Accounts' }
+                          :title => 'Billing Boss | Trading Accounts',
+                          :default_items_per_page => 10,
+                          :maximum_items_per_page => 100}
 
 end
--- trading-accounts/app/models/s_data/trading_account.rb
+++ trading-accounts/app/models/s_data/trading_account.rb
@@ -19,23 +19,23 @@ class SData::TradingAccount < Customer
     
   def payload_map(opts={})
     {
-      :name               => {:value => self.name,             :priority => 4},
-      :website            => {:value => self.website,          :priority => 1},
-      :currency           => {:value => self.currency,         :priority => 2},
-      :taxation_country   => {:value => self.taxation_country, :priority => 2},
-      :created_by_id      => {:value => self.created_by_id,    :priority => 3},
-      :address1           => {:value => self.address1,         :priority => 2},
-      :address2           => {:value => self.address2,         :priority => 2},
-      :city               => {:value => self.city,             :priority => 2},
-      :province_state     => {:value => self.province_state,   :priority => 2},
-      :postalcode_zip     => {:value => self.postalcode_zip,   :priority => 2},
-      :country            => {:value => self.country,          :priority => 2},
-      :phone              => {:value => self.phone,            :priority => 1},
-      :fax                => {:value => self.fax,              :priority => 1},
-      :simply_guid        => {:value => self.simply_guid,      :priority => 4},
-      :default_contact    => {:value => self.contacts[0],      :priority => 1},
-      :contacts           => {:value => self.contacts,         :priority => 1, :expand => (opts[:expand] || :link)},
-      :statuses           => {:value => ['hello', 'goodbye'],  :priority => 1}
+      :name               => {:value => self.name,              :priority => 4}, #test case for single string
+      :website            => {:value => self.website,           :priority => 1},
+      :currency           => {:value => self.currency,          :priority => 2},
+      :taxation_country   => {:value => self.taxation_country,  :priority => 2},
+      :created_by_id      => {:value => self.created_by_id,     :priority => 3}, #test case for single integer
+      :address1           => {:value => self.address1,          :priority => 2},
+      :address2           => {:value => self.address2,          :priority => 2},
+      :city               => {:value => self.city,              :priority => 2},
+      :province_state     => {:value => self.province_state,    :priority => 2},
+      :postalcode_zip     => {:value => self.postalcode_zip,    :priority => 2},
+      :country            => {:value => self.country,           :priority => 2},
+      :phone              => {:value => self.phone,             :priority => 1},
+      :fax                => {:value => self.fax,               :priority => 1},
+      :simply_guid        => {:value => self.simply_guid,       :priority => 4},
+      :default_contact    => {:value => self.contacts[0],       :priority => 1}, #test case of single activerecord
+      :contacts           => {:value => self.contacts,          :priority => 1, :expand => (opts[:expand] || :link)}, #test case for array of activerecords
+      :statuses           => {:value => ['status1', 'status2'], :priority => 1}  #test case for array of strings
     }
   end
 end
