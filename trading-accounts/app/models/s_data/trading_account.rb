2989944c5ec4f5d55598498673579e9fc2eb900c
eugene gilburg <eugene@richard-test2.(none)>
2010-04-16 18:33:35 -0700

    app update for extra condition testing

--- trading-accounts/app/models/s_data/trading_account.rb
+++ trading-accounts/app/models/s_data/trading_account.rb
@@ -6,7 +6,7 @@ class SData::TradingAccount < Customer
   end
 
   def sdata_content
-    "Customer ##{self.id}: #{self.name}"
+    "Trading Account ##{self.id}: #{self.name}"
   end
   
   # rough spec implementation suggestions:
@@ -19,23 +19,31 @@ class SData::TradingAccount < Customer
     
   def payload_map(opts={})
     {
-      :name               => {:value => self.name,              :priority => 4}, #test case for single string
-      :website            => {:value => self.website,           :priority => 1},
-      :currency           => {:value => self.currency,          :priority => 2},
-      :taxation_country   => {:value => self.taxation_country,  :priority => 2},
-      :created_by_id      => {:value => self.created_by_id,     :priority => 3}, #test case for single integer
-      :address1           => {:value => self.address1,          :priority => 2},
-      :address2           => {:value => self.address2,          :priority => 2},
-      :city               => {:value => self.city,              :priority => 2},
-      :province_state     => {:value => self.province_state,    :priority => 2},
-      :postalcode_zip     => {:value => self.postalcode_zip,    :priority => 2},
-      :country            => {:value => self.country,           :priority => 2},
-      :phone              => {:value => self.phone,             :priority => 1},
-      :fax                => {:value => self.fax,               :priority => 1},
-      :simply_guid        => {:value => self.simply_guid,       :priority => 4},
-      :default_contact    => {:value => self.contacts[0],       :priority => 1}, #test case of single activerecord
-      :contacts           => {:value => self.contacts,          :priority => 1, :expand => (opts[:expand] || :link)}, #test case for array of activerecords
-      :statuses           => {:value => ['status1', 'status2'], :priority => 1}  #test case for array of strings
+      :name                => {:value => self.name,              :priority => 4}, #test case for single string
+      :website             => {:value => self.website,           :priority => 1},
+      :currency            => {:value => self.currency,          :priority => 2},
+      :taxation_country    => {:value => self.taxation_country,  :priority => 2},
+      :created_by_id       => {:value => self.created_by_id,     :priority => 3}, #test case for single integer
+      :address1            => {:value => self.address1,          :priority => 2},
+      :address2            => {:value => self.address2,          :priority => 2},
+      :city                => {:value => self.city,              :priority => 2},
+      :province_state      => {:value => self.province_state,    :priority => 2},
+      :postalcode_zip      => {:value => self.postalcode_zip,    :priority => 2},
+      :country             => {:value => self.country,           :priority => 2},
+      :phone               => {:value => self.phone,             :priority => 1},
+      :fax                 => {:value => self.fax,               :priority => 1},
+      :simply_guid         => {:value => self.simply_guid,       :priority => 4},
+      :default_contact     => {:value => self.contacts[0],       :priority => 1}, #test case of single activerecord
+      :contacts            => {:value => self.contacts,          :priority => 1, :expand => (opts[:expand] || :link)}, #test case for array of activerecords
+      :statuses            => {:value => ['status1', 'status2'], :priority => 1},  #test case for array of strings
+      :simple_object_hash  => {:value => {:simple_object_key => 'simple_object_value'}, :priority => 1}, #test case for hash
+      :complex_object_hash => {:value => 
+                                 {:complex_object_key_1 => 'complex_object_key_1', 
+                                   :contacts => self.contacts, 
+                                   :single_contact => self.contacts[0]
+                                 },
+                               :priority => 1
+                              } #test case for complex hash
     }
   end
 end
