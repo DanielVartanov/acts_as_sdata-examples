370d1846f33324cdd89a6367215756c74c0cf417
eugene gilburg <eugene@richard-test2.(none)>
2010-04-23 16:11:53 -0700

    revert bad commit

--- trading-accounts/config/environment.rb
+++ trading-accounts/config/environment.rb
@@ -42,15 +42,5 @@ Rails::Initializer.run do |config|
   #TODO: move those out of here and into application.yml when it works
   $APPLICATION_URL = "http://www.billingboss.com"
   $SDATA_STORE_PATH = "/sdata/billingboss/crmErp/-/"
-
-  $SDATA_SCHEMAS = { 
-                     "crmErp"     => "http://schemas.sage.com/crmErp",
-                     "http"       => "http://schemas.sage.com/sdata/http/2008/1",
-                     "opensearch" => "http://a9.com/-/spec/opensearch/1.1",
-                     "sdata"      => "http://schemas.sage.com/sdata/2008/1",
-                     "sle"        => "http://www.microsoft.com/schemas/rss/core/2005",
-                     "xsi"        => "http://www.w3.org/2001/XMLSchema-instance"
-                   }
-  
-  
+ 
 end
