7f8e7813c5cb0bb83e34f73cab4f7446e1787bf3
eugene gilburg <eugene@richard-test2.(none)>
2010-05-04 21:16:48 -0700

    updated dummy app

--- trading-accounts/config/routes.rb
+++ trading-accounts/config/routes.rb
@@ -1,4 +1,5 @@
-ActionController::Routing::Routes.draw(:delimiters => ['/', '.', '!', '\$', '\(', '\)' ]) do |map|
+ActionController::Routing.module_eval "remove_const(:Routes); Routes = Usher::Interface.for(:rails23)"
+ActionController::Routing::Routes.draw(:delimiters => ['/', '.', '!', '\(', '\)' ]) do |map|
   map.sdata_resource :trading_accounts, :prefix => '/sdata/billingboss/crmErp/-/', :namespace => 's_data'
 
   map.connect ':controller/:action/:id'
